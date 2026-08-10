beca896bb4a1563299ef2ca289afa0176a3b4905
Andrés Muñoz <andres2078@gmail.com>
2026-08-10 11:11:54 -0500

    feat: add toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import PatientForm from "./components/PatientForm";
-import PatientsList from "./components/PatientsList";
+import { ToastContainer } from 'react-toastify';
+import PatientForm from './components/PatientForm';
+import PatientsList from './components/PatientsList';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -13,10 +15,10 @@ function App() {
         <div className="mt-12 md:flex">
           <PatientForm />
           <PatientsList />
-
         </div>
-
       </div>
+
+      <ToastContainer />
     </>
   );
 }
