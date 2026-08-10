d2556235cb99590cfbe659daa43bb913556af173
Andrés Muñoz <andres2078@gmail.com>
2026-08-10 10:55:43 -0500

    feat: update form submit label

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -139,7 +139,7 @@ export default function PatientForm() {
         <input
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-          value="Guardar Paciente"
+          value={activeId ? 'Actualizar Paciente' : 'Guardar Paciente'}
         />
       </form>
     </div>
