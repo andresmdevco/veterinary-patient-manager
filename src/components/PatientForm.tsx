e248ab3806f1d48215bfcd50ad4f484617efd090
Andrés Muñoz <andres2078@gmail.com>
2026-08-09 21:29:31 -0500

    feat: display patient list

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -19,7 +19,7 @@ export default function PatientForm() {
     <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
-      <p className="text-lg mt-5 text-center mb-10">
+      <p className="text-xl mt-5 text-center mb-10">
         Añade Pacientes y {''}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
