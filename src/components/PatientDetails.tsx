fa31599a02247f7a216be5136a7a0ffeb8ae0449
Andrés Muñoz <andres2078@gmail.com>
2026-08-09 22:39:03 -0500

    feat: add edit and delete patient buttons

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -14,6 +14,21 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
       <PatientDetailItem label="Email" data={patient.email} />
       <PatientDetailItem label="Fecha de alta" data={patient.date.toString()} />
       <PatientDetailItem label="Síntomas" data={patient.symptoms} />
+
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg cursor-pointer"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg cursor-pointer"
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 }
