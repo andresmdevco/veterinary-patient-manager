697462ffc65b537fa296848648aff87299ec1fdc
Andrés Muñoz <andres2078@gmail.com>
2026-08-16 13:26:03 -0500

    chore: update app title

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
     <>
       <div className="container mx-auto mt-20">
         <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
-          Seguimiento de Pacientes {''}
-          <span className="text-indigo-700">Veterinaria</span>
+          Control de Pacientes  {''}
+          <span className="text-indigo-700">Veterinarios</span>
         </h1>
 
         <div className="mt-12 md:flex">
